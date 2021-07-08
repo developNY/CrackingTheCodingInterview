76fd537dd39115ef891d5ca1013dc3ac7a45d124
developNY <sksldi0@gmail.com>
2021-07-08 14:36:33 +0900

    Q1_05 finish

--- src/Ch01_Arrays_and_Strings/Q1_05_One_Away/QuestionA.java
+++ src/Ch01_Arrays_and_Strings/Q1_05_One_Away/QuestionA.java
@@ -3,37 +3,40 @@ package Ch01_Arrays_and_Strings.Q1_05_One_Away;
 import java.util.Arrays;
 
 public class QuestionA {
-    public static boolean checkOneWay(String a, String b){
-        if(Math.abs(a.length()-b.length()) != 1)
-            return false;
-        if(a.length() > b.length()){
-            String tmp;
-            tmp = a;
-            a = b;
-            b = tmp;
-        }
+    public static boolean checkOneWay(String a, String b, int len){
         char[] a_arr = a.toCharArray(); //short
         char[] b_arr = b.toCharArray(); //long
         Arrays.sort(a_arr);
         Arrays.sort(b_arr);
         int b_cnt = 0;
         int diff = 0;
-        for (int i = 0; i < a.length(); i++) {
+        for (int i = 0; i < len; i++) {
             if(a_arr[i] != b_arr[b_cnt]){
                 diff++;
-                if(a_arr[i] != b_arr[b_cnt+1])
+                if(diff > 1 || a_arr[i] != b_arr[b_cnt+1])
                     return false;
                 b_cnt++;
             }
             b_cnt++;
         }
-        return diff < 2;
+        return true;
+    }
+
+    public static boolean oneEditAway(String a, String b){
+        if(Math.abs(a.length()-b.length()) > 2)
+            return false;
+        if(a.length() == b.length())
+            return checkOneWay(a, b, a.length()-1);
+        else if (a.length() > b.length())
+            return checkOneWay(b, a, b.length());
+        else
+            return checkOneWay(a, b, a.length());
     }
 
     public static void main(String[] args) {
-        String[][] pairs = {{"pale", "bake"}};
+        String[][] pairs = {{"pale", "ple"},{"pales", "pale"},{"pale", "bale"},{"pale", "bakse"}};
         for (String[] pair : pairs) {
-            System.out.println(checkOneWay(pair[0], pair[1]));
+            System.out.println(oneEditAway(pair[0], pair[1]));
         }
     }
 }
