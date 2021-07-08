8dd1a7ebda818d7ead34624bcf267a3912babd54
developNY <sksldi0@gmail.com>
2021-07-08 17:18:43 +0900

    Revise Q1_06

--- src/Ch01_Arrays_and_Strings/Q1_06_String_Compression/QuestionB.java
+++ src/Ch01_Arrays_and_Strings/Q1_06_String_Compression/QuestionB.java
@@ -1,17 +1,19 @@
 package Ch01_Arrays_and_Strings.Q1_06_String_Compression;
 
-public class QuestionA {
+public class QuestionB {
+    //Using StringBuilder
     public static String compressBad(String str){
-        String res = "";
+        StringBuilder sentence = new StringBuilder();
         int cnt = 0;
         for (int i = 0; i < str.length(); i++) {
             cnt++;
             if(i+1 >= str.length() || str.charAt(i) != str.charAt(i+1)){
-                res += str.charAt(i) + cnt;     //O(n^2)
+                sentence.append(str.charAt(i));
+                sentence.append(cnt);
                 cnt = 0;
             }
         }
-        return str.length() < res.length()? str : res;
+        return str.length() < sentence.length()? str : sentence.toString();
     }
 
     public static void main(String[] args) {
