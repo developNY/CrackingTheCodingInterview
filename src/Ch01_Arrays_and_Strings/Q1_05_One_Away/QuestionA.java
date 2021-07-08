abe05ffb83d07c5ee46ccbfbcd170eeea7eb89cc
developNY <sksldi0@gmail.com>
2021-07-08 14:07:54 +0900

    add, delete is okay, but replace has error

--- src/Ch01_Arrays_and_Strings/Q1_05_One_Away/QuestionA.java
+++ src/Ch01_Arrays_and_Strings/Q1_05_One_Away/QuestionA.java
@@ -4,13 +4,16 @@ import java.util.Arrays;
 
 public class QuestionA {
     public static boolean checkOneWay(String a, String b){
-        //add&delete&replace: check diff 1
-        //1 for-loop compare and count different value
-        //exception diff letter num > 2 X
-        if(Math.abs(a.length()-b.length())<2)
+        if(Math.abs(a.length()-b.length()) != 1)
             return false;
-        char[] a_arr = a.toCharArray();
-        char[] b_arr = b.toCharArray();
+        if(a.length() > b.length()){
+            String tmp;
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        char[] a_arr = a.toCharArray(); //short
+        char[] b_arr = b.toCharArray(); //long
         Arrays.sort(a_arr);
         Arrays.sort(b_arr);
         int b_cnt = 0;
@@ -18,19 +21,19 @@ public class QuestionA {
         for (int i = 0; i < a.length(); i++) {
             if(a_arr[i] != b_arr[b_cnt]){
                 diff++;
-                if()
+                if(a_arr[i] != b_arr[b_cnt+1])
+                    return false;
+                b_cnt++;
             }
             b_cnt++;
-
-
         }
-        return true;
+        return diff < 2;
     }
 
     public static void main(String[] args) {
-        String[][] pairs = {{"pale", "ple"}};
-        for (int i = 0; i < pairs.length; i++) {
-            System.out.println(checkOneWay(pairs[i][0], pairs[i][1]));
+        String[][] pairs = {{"pale", "bake"}};
+        for (String[] pair : pairs) {
+            System.out.println(checkOneWay(pair[0], pair[1]));
         }
     }
 }
