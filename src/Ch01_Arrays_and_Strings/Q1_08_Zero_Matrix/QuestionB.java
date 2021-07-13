e61bd5cf41f6d52e7daeeb88ab8cd70d02e0381d
developNY <sksldi0@gmail.com>
2021-07-13 17:41:58 +0900

    revision start

---
+++ src/Ch01_Arrays_and_Strings/Q1_08_Zero_Matrix/QuestionB.java
@@ -0,0 +1,18 @@
+package Ch01_Arrays_and_Strings.Q1_08_Zero_Matrix;
+
+public class QuestionB {
+    public static void setZero(int[][] matrix){
+        boolean rowHasZero = false;
+        boolean colHasZero = false;
+
+        //row 
+        for (int i = 0; i < matrix.length; i++) {
+            if()
+
+        }
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1,1,1,1},{1,1,1,1,1},{1,0,0,1,1},{1,1,1,1,1},{0,1,1,1,1}};
+        setZero(matrix);
+    }
+}
