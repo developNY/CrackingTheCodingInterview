f6efb04fa4505bda69198f3deef75608b90c0ba8
developNY <sksldi0@gmail.com>
2021-07-20 18:03:15 +0900

    Add CtClLibrary

---
+++ src/Ch02_Linked_Lists/Q2_02_Return_Kth_To_Last/QuestionA.java
@@ -0,0 +1,12 @@
+package Ch02_Linked_Lists.Q2_02_Return_Kth_To_Last;
+
+import CtCILibrary.AssortedMethods;
+import CtCILibrary.LinkedListNode;
+
+public class QuestionA {
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3, 4, 5, 6};
+        LinkedListNode head = AssortedMethods.createLinkedListFromArray(array);
+
+    }
+}
