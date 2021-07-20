eb1c10816373cedd15484ec27c55432e89129207
developNY <sksldi0@gmail.com>
2021-07-20 20:16:31 +0900

    Finish QuestionA

--- src/Ch02_Linked_Lists/Q2_02_Return_Kth_To_Last/QuestionB.java
+++ src/Ch02_Linked_Lists/Q2_02_Return_Kth_To_Last/QuestionB.java
@@ -3,23 +3,18 @@ package Ch02_Linked_Lists.Q2_02_Return_Kth_To_Last;
 import CtCILibrary.AssortedMethods;
 import CtCILibrary.LinkedListNode;
 
-public class QuestionA {
+public class QuestionB {
     public static int printKthToLast(LinkedListNode head, int k){
-        if(head == null){
-            return 0;
-        }
-        int index = printKthToLast(head.next, k) + 1;
-        if(index == k){
-            System.out.println(k + "th to last node is" + head.data);
-        }
-        return index;
+
     }
 
     public static void main(String[] args) {
         int[] array = {0, 1, 2, 3, 4, 5, 6};
         LinkedListNode head = AssortedMethods.createLinkedListFromArray(array);
-        for (int i = 0; i <= array.length + 1 ; i++) {
-            printKthToLast(head, i);
+        for (int i = 0; i <= array.length+1; i++) {
+            LinkedListNode node = kthToLast(head, i);
+            String nodeValue = node == null ? "null" : "" + node.data;
+            System.out.println(i + ": " + nodeValue);
         }
     }
 }
