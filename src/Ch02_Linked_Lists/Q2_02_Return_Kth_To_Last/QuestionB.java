7cde9baa0c14c9ae6d12d954cb02848a9b8620a0
developNY <sksldi0@gmail.com>
2021-07-21 22:25:14 +0900

    Start Qb

--- src/Ch02_Linked_Lists/Q2_02_Return_Kth_To_Last/QuestionB.java
+++ src/Ch02_Linked_Lists/Q2_02_Return_Kth_To_Last/QuestionB.java
@@ -4,8 +4,22 @@ import CtCILibrary.AssortedMethods;
 import CtCILibrary.LinkedListNode;
 
 public class QuestionB {
-    public static int printKthToLast(LinkedListNode head, int k){
+    //Wrapper Class
+    public static class Index{
+        public int value = 0;
+    }
+    //Use Wrapper
+    public static LinkedListNode kthToLast(LinkedListNode head, int k){
+        Index idx = new Index();
+        return kthToLast(head, k, idx);
+    }
 
+    public static LinkedListNode kthToLast(LinkedListNode head, int k, Index idx){
+        if(head == null){
+            return null;
+        }
+        LinkedListNode node = kthToLast(head.next, k, idx);
+        idx.value
     }
 
     public static void main(String[] args) {
