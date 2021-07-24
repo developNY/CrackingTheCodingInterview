b88041568a24b9771a8a64a03161f29d7c0b1890
developNY <sksldi0@gmail.com>
2021-07-24 17:36:23 +0900

    Finish Q2.03

--- src/Ch02_Linked_Lists/Q2_03_Delete_Middle_Node/QuestionA.java
+++ src/Ch02_Linked_Lists/Q2_03_Delete_Middle_Node/QuestionA.java
@@ -1,7 +1,21 @@
 package Ch02_Linked_Lists.Q2_03_Delete_Middle_Node;
 
-public class QuestionA {
-    public static void main(String[] args) {
+import CtCILibrary.AssortedMethods;
+import CtCILibrary.LinkedListNode;
 
+public class QuestionA {
+    public static LinkedListNode deleteNode(LinkedListNode head){
+        if(head == null || head.next == null)
+            return null;
+
+       head.next = head.next.next;
+       return head;
+    }
+
+    public static void main(String[] args) {
+        LinkedListNode head = AssortedMethods.randomLinkedList(10, 0, 10);
+        System.out.println("Before: " + head.printForward());
+        head = deleteNode(head);
+        System.out.println("After: " + head.printForward());
     }
 }
